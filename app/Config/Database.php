eb58ade74cb261afbaaec2e50b0834f062c9726d
Blandin Ilann <blandin.ilann@gmail.com>
2023-11-30 09:18:48 +0100

    db

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,7 +27,7 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'iblandin ',
+        'username'     => 'iblandin',
         'password'     => 'Dmnstrtr124-',
         'database'     => 'KSAV',
         'DBDriver'     => 'MySQLi',
